220f6d24aadfede6e32ef75098b1bc2fc5eeec53
hiralshukla <hiralshukla365@gmail.com>
2025-03-01 11:00:14 -0500

    Changed comment location

--- chat.py
+++ chat.py
@@ -7,13 +7,18 @@ import networking
 import stock_market as sm
 from socket import socket
 
-"""
-Stocks Module
-Author: Hiral Shukla (github.com/hiralshukla)
-Author: Shan Sundal (github.com/ssundal)
-Version: 1.0 - attempting to create a chat feature
+module_name = "Chat"
+module_command = "chat"
 
-"""
 
 def module(socket: socket, active_terminal: Terminal, pid: int, name: str):
+    """
+    Stocks Module
+    Author: Hiral Shukla (github.com/hiralshukla)
+    Author: Shan Sundal (github.com/ssundal)
+    Version: 1.0 - attempting to create a chat feature
+
+    """
     active_terminal.update("\033[0m     chats loading...", padding=False)
+
+
